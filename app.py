b4116f1e9e4d02b9bb0ae2e9b2b297dad5c4356b
Marcel Slootweg <mslootweg@viktor.ai>
2024-02-03 14:46:41 +0100

    decreased parametrization width, added user error, updated README

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from viktor import ViktorController, File
+from viktor import ViktorController, File, UserError
 from viktor.parametrization import ViktorParametrization, Text, FileField
 from viktor.views import IFCView, IFCResult
 
@@ -11,13 +11,15 @@ Let's make some steps in making IFC classification easier.
 
 ## Step 1: Upload your IFC file
     """)
-    ifc_file_source = FileField('Upload IFC file', file_types=['.ifc'])
+    ifc_file_source = FileField('Upload IFC file', file_types=['.ifc'], flex=100)
 
 
 class Controller(ViktorController):
     label = 'My Entity Type'
-    parametrization = Parametrization
+    parametrization = Parametrization(width=30)
 
     @IFCView('IFC view', duration_guess=1)
     def get_ifc_view(self, params, **kwargs):
+        if not params.ifc_file_source:
+            raise UserError("Make sure to upload an IFC file...")
         return IFCResult(params.ifc_file_source)
